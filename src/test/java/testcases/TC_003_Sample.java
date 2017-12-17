392c48c26bddf707aad6e5a6e1cee1e73f22c6f2
sanmativardhaman <34607157+sanmativardhaman@users.noreply.github.com>
2017-12-17 20:08:00 +0530

    Update TC_003_Sample.java

--- src/test/java/testcases/TC_003_Sample.java
+++ src/test/java/testcases/TC_003_Sample.java
@@ -5,5 +5,6 @@ public class TC_003_Sample
 public static void main(String[] args) {
 	System.out.println("new code");
 	System.out.println("adding new line to the code");
+	System.out.println(" this is to do pull");
 }
 }
